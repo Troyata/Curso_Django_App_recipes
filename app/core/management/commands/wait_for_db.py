4ab0712576da9d42265b19b59d039b0d6ef727e9
JoséT <josetroyacantos@gmail.com>
2024-05-20 23:13:30 +0200

    Version con el modelo de usuario customizado

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,7 +7,7 @@ import time
 
 from psycopg2 import OperationalError as Psycopg2Error
 
-from django.db.utils import OperationalError
+from django.db.utils import OperationalError  # type: ignore
 from django.core.management.base import BaseCommand  # type: ignore
 
 
--- app/core/models.py
+++ app/core/models.py
@@ -21,11 +21,22 @@ class UserManager(BaseUserManager):
          Por otro lado, extrafields te da flexibilidad puesto que puedes pasar
          otros argumentos sin necesidad de especificarlos desde el ppio. Util
          por si hay cambios."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         """ El set password se encarga de criptografíar el pass para almacenaje en BBDD"""
         user.save(using=self._db)
         
+        return user
+    
+    def create_superuser(self, email, password):
+        ''' Create a new superuser '''
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
